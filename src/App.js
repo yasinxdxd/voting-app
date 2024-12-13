c4616859c3e3b3dd0f8fb900e1e4b48952116f08
Melike Erdem <melikeerdem111@gmail.com>
2024-12-13 20:33:15 +0300

    profile page is created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignIn } from "./pages/SignIn.js";
 import { SignUp } from "./pages/SignUp.js";
 import { Home } from "./pages/Home.js";
+import { Profile } from "./pages/Profile.js";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/Profile.js
@@ -0,0 +1,7 @@
+export const Profile = () => {
+    return (
+        <div>
+            <h1>Welcome to the Profile Page</h1>
+        </div>
+    );
+}
